38ca7174fd09b2f6ef78d290d6f419e92a1279f3
Okko Ahonen <okko.a.ahonen@gmail.com>
2023-03-27 19:27:20 +0300

    Dashaus valmis

--- MirriPeli/Assets/Scripts/MovementScript.cs
+++ MirriPeli/Assets/Scripts/MovementScript.cs
@@ -8,6 +8,21 @@ public class MovementScript : MonoBehaviour
     public float speed;
     private Rigidbody2D rb2d;
 
+    private bool isFacingRight = true;
+    private float horizontal;
+    private float vertical;
+
+
+    //Helvetillinen m‰‰r‰ muuttujia "Dashaista" varten (puolet varmasti turhia)
+
+    private bool canDash = true;
+    private bool isDashing;
+    private float DashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+
+    [SerializeField] private TrailRenderer tr; //dashauksen taakse tulevan viivan saamiseksi
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -15,10 +30,54 @@ public class MovementScript : MonoBehaviour
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        if (isDashing) //Est‰‰ sen ettei pelaaja liiku samaan aikaan kun dashaa
+        {
+            return;
+        }
 
-        rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        rb2d.velocity = new Vector2(horizontal * speed, vertical * speed);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        Flip();
+
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false; //Varmistaa ettei Dashayksen aikana voi dashata
+        isDashing = true; //Aika selke‰
+
+        //rb2d.velocity = new Vector2(transform.localScale.x * DashingPower, 0f);
+        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * DashingPower;
+
+        tr.emitting = true; //Luo taakse taaksesi semmoseine hienon vanan
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false; //poistaa vanan
+        isDashing = false; //Aika selke‰
+
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+
+
 
     }
 
