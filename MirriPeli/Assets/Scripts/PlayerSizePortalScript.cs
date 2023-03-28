3b8b54458cd673863cc9d08090bd7ca859a32c10
kukkakakka <111349064+kukkakakka@users.noreply.github.com>
2023-03-28 13:15:38 +0300

    Added obama

--- MirriPeli/Assets/Scripts/PlayerSizePortalScript.cs
+++ MirriPeli/Assets/Scripts/PlayerSizePortalScript.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class PlayerSizePortalScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    private bool pieni = true;
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if(collision.CompareTag("kokoportaali"))
         {
             UnityEngine.Debug.Log("kokoportaali toimii");
-            transform.localScale = new Vector3(2 , 2 , 1);
+            if (pieni == true)
+            {
+                transform.localScale = new Vector3(2, 2, 1);
+                pieni= false;
+            }
+            else if (pieni == false) {
+                transform.localScale = new Vector3(1, 1, 1);
+                pieni= true;
+            }
+
         }
     }
 }
