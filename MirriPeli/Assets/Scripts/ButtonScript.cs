34b459db204641c7c071fc2a373dc308e744fece
alekaixd <aleksi.pirttinen@gmail.com>
2023-03-29 12:34:49 +0300

    added sprite to button

--- MirriPeli/Assets/Scripts/ButtonScript.cs
+++ MirriPeli/Assets/Scripts/ButtonScript.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class ButtonScript : MonoBehaviour
 {
     public GameObject otherObject;
+    private Sprite originalSprite;
+    public Sprite pressedDownSprite;
     private int numberOfCollisions;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        originalSprite = spriteRenderer.sprite;
     }
 
     // Update is called once per frame
@@ -25,6 +29,7 @@ public class ButtonScript : MonoBehaviour
         if (numberOfCollisions == 1)
         {
             otherObject.SetActive(false);
+            spriteRenderer.sprite = pressedDownSprite;
         }
         
     }
@@ -35,6 +40,7 @@ public class ButtonScript : MonoBehaviour
         if (numberOfCollisions == 0)
         {
             otherObject.SetActive(true);
+            spriteRenderer.sprite = originalSprite;
         }
     }
 }
