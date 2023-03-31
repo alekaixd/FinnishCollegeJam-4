c3e2f38a709029dbdb637396459468bae079f9cb
alekaixd <aleksi.pirttinen@gmail.com>
2023-03-31 05:28:31 +0300

    final pushes
    it is 5.30am right now

---
+++ MirriPeli/Assets/EndGame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine("endGame");
+    }
+
+    private IEnumerator endGame()
+    {
+        yield return new WaitForSeconds(11.0f);
+        SceneManager.LoadScene("Mainmenu");
+    }
+}
